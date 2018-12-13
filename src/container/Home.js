60ddfafddc44deb7cd4f3891cc5a7e608b3881e0
Sandeepmckinley <sandeep.yadav@mckinleyrice.com>
2018-12-13 16:43:42 +0530

    Home page design in react

---
+++ src/container/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import {MainPage} from '../components/MainPage';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+export const Home = ()=>{
+    return(
+        <div className="main-page">
+        <MainPage/>
+        </div>
+    )
+}
