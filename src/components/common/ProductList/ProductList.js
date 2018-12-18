c8819e11260150e54626885203d466fc957983ec
savagemanage <janghoonlee@mckinleyrice.com>
2018-12-18 02:50:56 -0800

    Add demo GET Request to Infogreen REST API and render products

--- src/components/common/ProductList/ProductList.js
+++ src/components/common/ProductList/ProductList.js
@@ -1,81 +1,51 @@
 import React from 'react';
-import ProductCard from '../../common/ProductCard/ProductCard';
-
 import { Card } from 'semantic-ui-react';
-import 'semantic-ui-css';
+import ProductCard from '../../common/ProductCard/ProductCard';
+import axios from 'axios';
 
+import 'semantic-ui-css';
 import styles from '../../common/CommonCSS/CommonCSS.css';
 import fabric1 from '../../../assets/images/fabric1.jpg';
 
-export const ProductList = (props) => {
-    return(
-        <div className={styles.productHeading}>
-            <div className="tip-heading">
-                <i class="fa fa-heart" aria-hidden="true"></i>
-                <h4>{props.category}</h4>
-            </div>
-            <div className={styles.rightDiv}>
-                <div className="row">
-                    <Card.Group itemsPerRow={4}>
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                        <ProductCard 
-                            src={fabric1}
-                            category={"주방세제"}
-                            name={"어떤 세제"}
-                            description={"어떤 설명"}
-                            rating={3}
-                        />
-                    </Card.Group>
+export class ProductList extends React.Component {
+    state = {
+        chemicals: [],
+    }
+
+    componentDidMount() {
+        axios.get("http://13.125.89.0/chemical/items_limit.php")
+            .then(res => {
+                const products = res;
+                this.setState({ chemicals: products.data[0] });
+                console.log(products.data[0][0].image);
+            })
+    }
+    
+    render () {
+        return(
+            <div className={styles.productHeading}>
+                <div className="tip-heading">
+                    <i class="fa fa-heart" aria-hidden="true"></i>
+                    <h4>{this.props.category}</h4>
+                </div>
+                <div className={styles.rightDiv}>
+                    <div className="row">
+                        <Card.Group itemsPerRow={4}>
+                        { 
+                            this.state.chemicals.map((chemical, index) =>
+                            <ProductCard 
+                                src={"http://13.125.89.0/chemical/item_img/" + chemical.image}
+                                category={chemical.category}
+                                name={chemical.name}
+                                description={chemical.brand}
+                                rating={chemical.star}
+                            />
+                        )}
+                            
+                        </Card.Group>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
