e2db48c8ad10ba6a141e0a5e2c651afffa9786a3
Harsh <wardhanster@gmail.com>
2018-12-21 13:18:39 +0530

    Made search component configurable - heading & placeholder

--- src/components/content/search/Search.js
+++ src/components/content/search/Search.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Search.css';
 
-export const Search =()=>{
+export const Search =(props)=>{
     return(
         <div className="search-div">
             <div className="container">
                 <div className="search_box">
-                    <h1>Lorem Ipsum Heading</h1>
-                    <input type="text" placeholder="검색 예) 세제"/>
+                    <h1>{props.heading||'Lorem Ipsum Heading'}</h1>
+                    <input type="text" placeholder={props.placeholder||'검색 예) 세제'}/>
                     <i className="fa fa-search" aria-hidden="true"></i>
                 </div>
             </div>
