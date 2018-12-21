55b679b8f410c0ff3162319317e66725f5d6ce91
Priya Jain <priya0607jain@gmail.com>
2018-12-22 00:52:19 +0530

    tips page css and design fixes

--- src/components/common/StaticCard/StaticCard.js
+++ src/components/common/StaticCard/StaticCard.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import './StaticCard.css'
+
 
 const StaticCard = () => (
   <Card>
     <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
-    <Card.Content>
+    {/* <Card.Content>
       <Card.Header>Matthew</Card.Header>
       <Card.Meta>
         <span className='date'>Joined in 2015</span>
       </Card.Meta>
       <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
-    </Card.Content>
+    </Card.Content> */}
     <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
+      <h6><b>Main Heading</b></h6>
+      <p>Some related content</p>
+      <Button style={{float:'right', backgroundColor: '#5dade2'}}>View</Button>
     </Card.Content>
   </Card>
 )
--- src/components/content/navigation/tips/Tips.js
+++ src/components/content/navigation/tips/Tips.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import './Tips';
+import './Tips.css';
+
+import StaticCard from '../../../common/StaticCard/StaticCard';
+import { Search } from '../../search/Search';
+import Paginate from '../pagination/pagination';
 
 
 export const Tips = () => {
     return (
-        <div className="container">
+        <div className="container-fluid" style={{marginTop: 100}}>
             <div className="row">
                 <div className="col-sm-1">
                 </div>
 
                 <div className="col-md-2 tipsBannnerOne">
+                    <div style={{position: 'absolute', bottom: 0}}>
                         <h5><b>Product Name</b></h5>
-                        <h6><Link to='#'>see more</Link></h6>
+                        <p>Some Related Text</p>
+                        <h6><Link to='#'>see more ></Link></h6>
+                    </div>
+                        
                 </div>
 
                 <div className="col-md-2 tipsBannnerTwo">
@@ -31,6 +39,44 @@ export const Tips = () => {
                 </div>
 
             </div>
+
+            <div className="row">
+                <div className="col-md-12" style={{marginTop: 50, marginRight: 50}}>
+                    <Search />
+                </div>
+
+            </div>
+
+            
+            <div className="row card-center">
+                <div className="col-md-6">
+                    <StaticCard />
+                </div>
+                
+                <div className="col-md-6">
+                    <StaticCard />
+                </div>
+            </div>
+
+            <div className="row card-center">
+                <div className="col-md-6">
+                    <StaticCard />
+                </div>
+                
+                <div className="col-md-6">
+                    <StaticCard />
+                </div>
+        
+            </div>
+
+            <div className="row">
+                <div className="col-md-12 center-block">
+                    <Paginate />
+                </div>
+            
+            </div>
+            
+            
         </div>
     )
 }
