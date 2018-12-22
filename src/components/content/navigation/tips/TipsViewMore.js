bbf6089a4bf456731dade530dc7feeeed7a50b44
Priya Jain <priya0607jain@gmail.com>
2018-12-22 13:11:06 +0530

    slider page UI fixed

--- src/components/content/navigation/tips/TipsViewMore.js
+++ src/components/content/navigation/tips/TipsViewMore.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
-import { Image, Input  } from 'semantic-ui-react';
+import { Image, Form, TextArea, Button  } from 'semantic-ui-react';
 import './Tips.css';
-import DescriptionSlider from '../../slider/DescriptionSlider';
+// import DescriptionSlider from '../../slider/DescriptionSlider';
+import {Comments} from '../../product/prodSpec/CommentsSection';
+import Paginate from '../pagination/pagination';
+
 
 export default class TipsViewMore extends Component {
   render() {
@@ -23,29 +26,38 @@ export default class TipsViewMore extends Component {
                     <Image src= 'http://www.skillings.com/wp-content/uploads/2017/02/placeholder-square.jpg' size='large' centered className="img-large" />  
                 </div>
             </div>
-            
-            <div className="row" style={{marginTop: 150, marginBottom: 100}}>
-                <div className="col-md-12" style={{backgroundColor: '#c2f0f0', height: 200, position: "absolute"}}>
-                   
+
+
+            <div className="row">
+                <div className="col-md-12" style={{backgroundColor: '#c2f0f0', height: 200, marginTop: 80}}>
+
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-12" style={{marginLeft: '22%', marginBottom: '5%', marginTop: '10%'}}>
+                    <h6><b>Some Text</b>&nbsp;&nbsp;&nbsp;&nbsp;<span>2018/00/00</span></h6>
+                    
+                    <Form style={{width: 770}}>
+                        <TextArea placeholder='Tell us more' rows="2"/>
+                        <Button style={{float:'right', backgroundColor: 'rgb(96, 179, 162)', marginTop: 20}}>Submit</Button>
+                    </Form>
+                    
+                </div>
+                
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <Comments />
                     
                 </div>
             </div>
 
             <div className="row">
-                <div className="col-md-12 slider-above">
-                  
-                        <h5>Some heading for the slider</h5>
-                        <DescriptionSlider />
-
-                </div>
-            </div>
-
-            <div className="row"  style={{marginTop: 100}}>
                 <div className="col-md-12">
-                <p><b>Some another text below</b></p>
-                <p>2018/00/00</p>
-                <Input placeholder='Lorem Ipsum is simply dummy text of the printing and typesetting industry' />
+                    <Paginate />
                 </div>
+
             </div>
         </div>
 
