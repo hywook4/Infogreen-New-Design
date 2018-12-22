489f060f78eb985c7931dd0ae63ef956751c553a
Priya Jain <priya0607jain@gmail.com>
2018-12-22 22:47:40 +0530

    fixed css staticcardslider

--- src/components/common/StaticCard/StaticCardSlider.js
+++ src/components/common/StaticCard/StaticCardSlider.js
@@ -5,16 +5,10 @@ import './StaticCardSlider.css'
 const StaticCardSlider = () => (
   <Card>
     <Image src='http://www.skillings.com/wp-content/uploads/2017/02/placeholder-square.jpg' />
-    {/* <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
-    </Card.Content> */}
+    
     <Card.Content extra>
       <h6><b>Main Heading</b></h6>
-      <p>Some related content</p>
+      <p>Some Related Content</p>
       <Button style={{float:'right', backgroundColor: '#5dade2'}}>View</Button>
     </Card.Content>
   </Card>
